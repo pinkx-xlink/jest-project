65c8a5dc9b8e9103455654294629d504c453d0dc
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-11 08:13:24 -0400

    ADD: create a test for a null variable

--- sum.test.js
+++ sum.test.js
@@ -21,3 +21,12 @@ test('adding positive numbers is not zero', () => {
         }
     }
 });
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
