73e5970a0d485a9fe901159d39933880904b1683
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-13 08:22:33 -0400

    Write a second test for capitalize()

--- capitalize.test.js
+++ capitalize.test.js
@@ -5,3 +5,7 @@ test('capitalizes the first letter of a word', () => {
     expect(capitalize('hello')).toBe('Hello');
 });
 
+test('capitalizes the first letter of "yellow" so it becomes "Yellow"', () => {
+    expect(capitalize('yellow')).toBe('Yellow');
+});
+
