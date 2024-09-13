f5dec441693f84e253118ecb8b848e643d196a92
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-13 11:43:50 -0400

    Refactor average variable

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,13 +1,11 @@
 function analyzeArray([array]) {
     const arrLength = array.length;
-
-    let average;
     let sum = 0;
     for (let i = 0; i < array.length; i++) {
         sum += array[i];
         return sum;
     }
-    average = (sum / arrLength);
+    const average = (sum / arrLength);
 
     const min = Math.min(...array);
     const max = Math.max(...array);
