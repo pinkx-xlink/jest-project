318a894d30e907e364f7dc4a2b34f657c9c27647
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-13 08:43:51 -0400

    Write a test for subtract()

--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,11 @@
 const { experiments } = require('webpack');
 const sum = require('./sum');
+const subtract = require('./subtract');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 3 - 2 to equal 1', () => {
+    expect(subtract(3 - 2)).toBe(1);
 });
