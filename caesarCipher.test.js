a8f398612c7cd6bc63518715f780e9fc86675567
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-13 10:31:42 -0400

    Add two additional tests that use punctuation and capitalization

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -4,3 +4,11 @@ const caesarCipher = require('./caesarCipher');
 test('Refactors a string using caesar cipher', () => {
     expect(caesarCipher('xyz', 3)).toBe('abc');
 });
+
+test('Refactors a capitalized string using caesar cipher', () => {
+    expect(caesarCipher('HeLLO', 3)).toBe('KhOOr');
+});
+
+test('Refactors a punctuated string using caesar cipher', () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zurog!');
+});
