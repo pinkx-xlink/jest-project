efd1c7216d0cc39a56be99f45a7555ceaaeef78e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-13 10:44:06 -0400

    Create the first test for analyzeArray()

---
+++ analyzeArray.test.js
@@ -0,0 +1,6 @@
+const { experiments } = require('webpack');
+const analyzeArray = require('./analyzeArray');
+
+test('Refactors a punctuated string using caesar cipher', () => {
+    expect(analyzeArray([1,8,3,4,2,6]).toBe(4, 1, 8, 6));
+});
