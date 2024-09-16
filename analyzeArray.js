5b87999e0b7ecb906a3945421af1580ede54df86
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-16 13:56:19 -0400

    Create getSum() to put the for loop in

--- analyzeArray.js
+++ analyzeArray.js
@@ -2,16 +2,20 @@ function analyzeArray([array]) {
     const arrLength = array.length;
     let sum = 0;
     // INVALID LOOP - NEEDS DEBUGGED
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-        return sum;
+    function getSum() {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+            return sum;
+        }
+        getSum();
     }
+    
     const average = (sum / arrLength);
 
     const min = Math.min(...array);
     const max = Math.max(...array);
 
-    return [average, min, max, arrLength];
+    return [array.average, array.min, array.max, array.arrLength];
 }
 
 module.exports = analyzeArray;
