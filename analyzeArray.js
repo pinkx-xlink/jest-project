9f11e03762555f5e951de19085220cd155487b8e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-16 14:59:51 -0400

    Refactor the sum variable and remove the for loop previously used to find the sum.

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,17 +1,14 @@
+const { curry } = require("lodash");
+
 function analyzeArray([array]) {
-    let ourArray = array;
-    const arrLength = array.length;
-    let sum = 0;
-    // INVALID LOOP - NEEDS DEBUGGED
-    function getAverage() {
-        for (let i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        getAverage();
-        return sum;
-    }
     
-    const average = (sum / arrLength);
+    const arrLength = array.length;
+
+    const sum = array.reduce((current, previous) => {
+        return current + previous;
+    });
+    const average = (sum / array.length);
+    
     const min = array.reduce((current, previous) => {
         return Math.min(current, previous);
     });
